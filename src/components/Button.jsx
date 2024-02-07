7ccb8392604f70b706ed75dca1d578c098b3de36
João Rodrigues <johnnyrod4569@gmail.com>
2024-02-07 14:53:18 +0100

    learn: Add dynamic conditional stayling with tailwind for the Input component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,6 +3,8 @@ export default function Button({children, ...props}) {
     <button
       className="px-4 py-2 font-semibold uppercase rounded text-stone-900 bg-amber-400 hover:bg-amber-500"
       {...props}
-    >{children}</button>
+    >
+      {children}
+    </button>
   );
 }
